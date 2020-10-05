109d17e31a178237abd718500fd7c83f45f96c62
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-05 22:04:25 +0100

    Progressive roll on strafe with restore to level flight set up - stop overrolling code in progress

--- Assets/scripts/RacerMovement.cs
+++ Assets/scripts/RacerMovement.cs
@@ -9,18 +9,28 @@ public class RacerMovement : MonoBehaviour
     public Transform racermodel;
   
     public float speed;
+    public float boost;
     public float dodge;
-    public float lift;
+    // public float lift;
+    public float rotate;
+  
+
+    
     
     
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+       
+           
+       
+         if (Input.GetKey(KeyCode.Space))
         {
+             racer.AddForce (transform.forward * boost * speed);
+        } else {
              racer.AddForce (transform.forward * speed);
         }
 
-        if (Input.GetKey(KeyCode.W))
+       /*  if (Input.GetKey(KeyCode.W))
         {
              racer.AddForce (transform.up * lift);
         }
@@ -28,18 +38,39 @@ public class RacerMovement : MonoBehaviour
           if (Input.GetKey(KeyCode.S))
         {
              racer.AddForce (-transform.up * lift);
-        }
+        }  */
 
         if (Input.GetKey(KeyCode.A))
         {
-            racermodel.Rotate (new Vector3(0, 0, 1) * Time.deltaTime * speed *0.5f, Space.World); 
-            racer.AddForce (-transform.right * dodge);
+             racer.AddForce (-transform.right * dodge);
+            if(racermodel.rotation.z <=35 )
+            {
+              Debug.Log ("I can turn");
+               racermodel.Rotate (new Vector3(0, 0, 1) * Time.deltaTime * rotate , Space.World); 
+            } else
+             {
+                     Debug.Log ("no more turn for me");
+            }
         }
+        
+        if (Input.GetKey(KeyCode.A) == false && racermodel.rotation.z >0)
+        {
+            racermodel.Rotate (new Vector3(0, 0, -1) * Time.deltaTime * rotate , Space.World); 
+        }
+
 
          if (Input.GetKey(KeyCode.D))
         {
-            racermodel.Rotate (new Vector3(0, 0, -1) * Time.deltaTime * speed *0.5f, Space.World);
-            racer.AddForce (transform.right * dodge);
+               racer.AddForce (transform.right * dodge);
+               if (racermodel.rotation.z >=-35)
+               {
+               racermodel.Rotate (new Vector3(0, 0, -1) * Time.deltaTime * rotate, Space.World);
+               }
+        }
+
+        if (Input.GetKey(KeyCode.D) == false && racermodel.rotation.z <0)   
+        {
+            racermodel.Rotate (new Vector3(0, 0, 1) * Time.deltaTime * rotate , Space.World); 
         }
 
     }
