b0503a26caac16db80593afb71010391fab8f8a8
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-12 22:21:14 +0100

    Started on runner destruction on collision - still to twitchy on ramps

---
+++ Assets/destroyrunner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyrunner : MonoBehaviour
+{
+    
+    public GameObject racerobject;
+    public GameObject teleportexit;
+     private void OnTriggerEnter(Collider other)
+    {
+    racerobject.transform.position = teleportexit.transform.position ;
+    
+    }
+
+}
--- Assets/triggeranimation.cs
+++ Assets/triggeranimation.cs
@@ -6,8 +6,8 @@ public class triggeranimation : MonoBehaviour
 {
     public Animator anim;
     
-    // Update is called once per frame
-    private void OnTriggerEnter(Collider other)
+    
+        private void OnTriggerEnter(Collider other)
     {
     anim.SetTrigger("triggercollapse");
     }
