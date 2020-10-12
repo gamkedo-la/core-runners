513ae8de74ecd87e3a4fa58cfc59e5455e7e5bc6
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-12 21:40:18 +0100

    Initial bridge destruction with proxy geometry for gorge - trigger on leave tunnel

---
+++ Assets/triggeranimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggeranimation : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+    anim.SetTrigger("triggercollapse");
+    }
+}
