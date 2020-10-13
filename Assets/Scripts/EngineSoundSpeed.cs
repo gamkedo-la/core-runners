9e76311e232885be349bc7c69abecaf875eeb036
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-13 02:04:06 -0700

    Engine sound effect

---
+++ Assets/Scripts/EngineSoundSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineSoundSpeed : MonoBehaviour
+{
+
+    public AudioSource engine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        engine.pitch = GetComponent<RacerMovement>().speed / 200;
+
+    }
+}
