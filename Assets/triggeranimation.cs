44315faed72fb6650bf4d01fde164d4a7b631b74
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-13 20:35:09 +0100

    Block out of bridge collapse over three gorges - random one drops - appropriate collider activated

--- Assets/triggeranimation.cs
+++ Assets/triggeranimation.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class triggeranimation : MonoBehaviour
 {
-    public Animator anim;
-    
-    
+    public Animator anim1;
+    public Animator anim2;
+    public Animator anim3;
+    public Collider col1;
+    public Collider col2;
+    public Collider col3;
         private void OnTriggerEnter(Collider other)
     {
-    anim.SetTrigger("triggercollapse");
+    //do this properly so can have 1 or 2 drop and think about resetting after crash
+    // can change values to weight probability of different collapses
+    var rand = Random.Range(0f,3f);
+    if (rand <=1){
+    anim1.SetTrigger("triggercollapse");
+    col1.enabled = true;
+    } else 
+    if (rand <=2){
+    anim2.SetTrigger("triggercollapse");
+    col2.enabled = true;
+    } else
+    anim3.SetTrigger("triggercollapse");
+    col3.enabled = true;
     }
 }
