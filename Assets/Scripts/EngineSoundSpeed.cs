fe1d2110b76614820ae9de81a6aff41a7e397a25
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2020-10-13 17:21:30 -0700

    speeds up engine sound when boosting

--- Assets/Scripts/EngineSoundSpeed.cs
+++ Assets/Scripts/EngineSoundSpeed.cs
@@ -7,18 +7,50 @@ public class EngineSoundSpeed : MonoBehaviour
 
     public AudioSource engine;
 
+    public BoostButtonPress boostButton;
+
+    float t = 0;
+
+    public float p = 1;
+    public float min = 1.0f;
+    public float max = 1.2f;
+
+    public bool reset;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        engine.pitch = GetComponent<RacerMovement>().speed / 200;
+        if (boostButton.boostbuttonPressed == true)
+        {
+
+            //if(p == min) { t = 0; }
+
+            //p = Mathf.Lerp(min, max, t);
+            //t += Time.deltaTime;
+
+            p = max;
+
+        }
+        if (boostButton.boostbuttonPressed == false)
+        {
+            // if (p == max) { t = 0; }
+
+            //engine.pitch = Mathf.Lerp(max, min, t);
+            //t += Time.deltaTime;
+
+            p = min;
+
+        }
+
+        engine.pitch = p;
 
     }
 }
