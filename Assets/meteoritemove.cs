b47a273341c9ac6a7cf8fe523ef8ecc3ad843277
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-18 22:54:06 +0100

    Current state prior to trying toon outline look

---
+++ Assets/meteoritemove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteoritemove : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+         transform.Translate(0, -Time.deltaTime * 350f, 0);
+    }
+}
