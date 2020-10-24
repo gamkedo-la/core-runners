a53371ee5f2111ae3492e6a0ae44d0a57081c7d1
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-24 17:55:05 +0100

    Flipbook particle materials for meteorite hit and collapse dust - tweaked some settings to get batching working again

--- Assets/Scripts/triggeranimation.cs
+++ Assets/Scripts/triggeranimation.cs
@@ -14,24 +14,35 @@ public class triggeranimation : MonoBehaviour
     public GameObject meteorite1;
     public GameObject meteorite2;
     public GameObject meteorite3;
+    public GameObject Dustcollapse2;
 
            private void OnTriggerEnter(Collider other)
     {
     //do this properly so can have 1 or 2 drop and think about resetting after crash
     // can change values to weight probability of different collapses
     var rand = Random.Range(0f,3f);
-    if (rand <=1){
+    if (rand <=0){
     anim1.SetTrigger("triggercollapse");
     col1.enabled = true;
     meteorite1.SetActive(true);
     } else 
-    if (rand <=2){
+    if (rand <=3){
     anim2.SetTrigger("triggercollapse");
     col2.enabled = true;
      meteorite2.SetActive(true);
+    StartCoroutine(Dustcollapse());
+
+
     } else
     anim3.SetTrigger("triggercollapse");
     col3.enabled = true;
      meteorite3.SetActive(true);
     }
+
+    IEnumerator Dustcollapse()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Dustcollapse2.SetActive(true);
+    }
+
 }
