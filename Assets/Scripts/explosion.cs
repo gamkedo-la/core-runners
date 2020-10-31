88eea15f43e79d1b936732ca809883e6e6b85e01
deepbluezen <deepbluezen@users.noreply.github.com>
2020-10-31 20:28:56 +0000

    Added explosion force to meteorite hit

---
+++ Assets/Scripts/explosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    
+    public float radius = 5.0f;
+    public float power = 10.0f;
+
+    public float upwardforce = 1.0f;
+           void Start()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, upwardforce);
+        }
+    }
+
+    
+}
