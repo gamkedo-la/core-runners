f63fc8068605ee72c4090d79f04911735f9d1630
roc_lee-at-051390139653 <7pXtrEhz3Jpf4cfMqucFt9tct2GdDETr5qMSwVnaYOk>
2020-11-10 15:10:47 -0500

    temporary powerup playback solution

---
+++ Assets/AudioSourceController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour
+{
+    [SerializeField] AudioSource[] sources;
+    [SerializeField] int sourcesToCreate;
+    int nextSourceIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateAudioSourceArray();
+    }
+    void CreateAudioSourceArray()
+    {
+        sources = new AudioSource[sourcesToCreate];
+        for (int i = 0; i < sourcesToCreate; i++)
+        {
+            sources[i] = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public AudioSource GetNextSource()
+    {
+        nextSourceIndex = (nextSourceIndex + 1) % sourcesToCreate;
+        return sources[nextSourceIndex];
+    }
+
+
+}
---
+++ Assets/Scripts/AudioOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class AudioOnTrigger : MonoBehaviour
+{
+    public AudioSourceController controller;
+    public AudioClip[] audioClips;
+    public AudioSource source;
+    [SerializeField] int next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (source == null)
+        //{
+        //    source = gameObject.AddComponent<AudioSource>();
+        //}
+
+        controller = FindObjectOfType<AudioSourceController>();
+        source = controller.GetNextSource();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        next = Random.Range(0, audioClips.Length - 1);
+        source = controller.GetNextSource();
+        source.spatialBlend = 0;
+        source.PlayOneShot(audioClips[next]);
+    }
+}
