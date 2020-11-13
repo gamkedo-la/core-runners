7c26617e66d0f79e11871916b9aa944fb17d1b73
roc_lee-at-051390139653 <7pXtrEhz3Jpf4cfMqucFt9tct2GdDETr5qMSwVnaYOk>
2020-11-13 11:45:32 -0500

    Creating music data object to feed music related trigger events, creating editor functions for editing music data

---
+++ Assets/Scripts/Editor/MusicDataEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MusicData))]
+public class MusicDataEditor : Editor
+{
+    MusicData data;
+    Vector2 pos;
+    bool displayData;
+
+    public void OnEnable()
+    {
+        data = (MusicData)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (data.beats.Count == 0)
+        {
+            EditorGUILayout.HelpBox("Empty Music Track Data", MessageType.Info);
+
+            if (GUILayout.Button("Generate Beats", EditorStyles.miniButton))
+                data.CreateBeatList();
+        }
+        else
+        {
+            if (GUILayout.Button("Reset Beats", EditorStyles.miniButton))
+                data.CreateBeatList();
+        }
+
+        displayData = EditorGUILayout.Foldout(displayData, "Display Beats");
+        if (displayData)
+        {
+            pos = EditorGUILayout.BeginScrollView(pos);
+
+            for (int i = 0; i < data.beats.Count; i++)
+            {
+                EditorGUILayout.LabelField("Beat# " + i.ToString());
+                data.beats[i] = EditorGUILayout.IntSlider(data.beats[i], -1, data.GetEventTypesRange() - 1);
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        EditorUtility.SetDirty(data);
+    }
+}
---
+++ Assets/Scripts/MusicData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicEvent
+{
+    [SerializeField] string name;
+    [SerializeField] int eventIdentifier;
+    [SerializeField] List<AudioClip> sounds;
+    [SerializeField] GameObject objectToTrigger; //Todo Make this an Interface
+}
+
+[CreateAssetMenu(menuName = "CoreRunners/MusicData", fileName = "New Music Data.asset")]
+public class MusicData : ScriptableObject
+{
+    [SerializeField] AudioClip musicTrack;
+    [SerializeField] int bpm;
+    [SerializeField] int beatCountInTrack;
+    //[SerializeField] int numberOfEventTypes;
+
+    [SerializeField] List<MusicEvent> eventTypes;
+
+    [HideInInspector] public List<int> beats = new List<int>();
+
+    public int GetEventTypesRange()
+    {
+        return eventTypes.Count;
+    }
+
+    public void CreateBeatList()
+    {
+        beats = new List<int>();
+
+        for (int beat = 0; beat < beatCountInTrack; beat++)
+        {
+            beats.Add(-1);
+        }
+    }
+}
