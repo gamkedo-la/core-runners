be26fad70883a62a12d21d990ed22e2cc5cd9f53
deepbluezen <deepbluezen@users.noreply.github.com>
2020-11-15 19:36:17 +0000

    Gate open animation and trigger

---
+++ Assets/Scripts/GateOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateOpen : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log ("Gate trigger");
+        anim.enabled = true;
+
+    }
+
+
+
+
+}
