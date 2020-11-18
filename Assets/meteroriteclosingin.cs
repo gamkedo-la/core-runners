317f9757c1d97acc75b7a316059090a5b56f4df9
deepbluezen <deepbluezen@users.noreply.github.com>
2020-11-18 21:49:51 +0000

    Stars added to sky - incoming metorite added - slowly increases in size over level

---
+++ Assets/meteroriteclosingin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteroriteclosingin : MonoBehaviour
+{
+    
+    
+    public float speed;
+
+    void Update()
+    {
+        
+       transform.localScale += Vector3.one * speed * Time.deltaTime;
+
+    }
+}
